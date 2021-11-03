3607b733a65d38ccc609cc4f77c73bc28dc19657
Yamada <ymd_h@outlook.jp>
2021-11-03 13:59:45 +0900

    Add install files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(name="well-behaved-logging",
+      author="Yamada Hiroyuki",
+      version="0.0.1",
+      packages=find_packages("."),
+      classifiers=["Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "Topic :: Software Development :: Libraries"])
